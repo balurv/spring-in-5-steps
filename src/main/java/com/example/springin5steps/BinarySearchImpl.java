d2cd0b69dc15db5123163bcb61529d478acfab57
balaji r v <balurv1997@gmail.com>
2023-01-06 15:12:40 +0530

    added scope for bean

--- src/main/java/com/example/springin5steps/BinarySearchImpl.java
+++ src/main/java/com/example/springin5steps/BinarySearchImpl.java
@@ -1,18 +1,25 @@
 package com.example.springin5steps;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class BinarySearchImpl {
 	
 	@Autowired
-	private SortAlgorithm sortAlgorithm;	
-
+	private SortAlgorithm quickSortAlgorithm;	
+	@Autowired
+	private SortAlgorithm bubbleSortAlgorithm;
+	
 	public int binarySearch(int[] arr, int number) {
 		
-		int[] sortedNumbers = sortAlgorithm.sort(arr);
-		System.out.println(sortAlgorithm);
+		int[] sortedNumbers = quickSortAlgorithm.sort(arr);
+		System.out.println(quickSortAlgorithm);
+		
+		System.out.println(bubbleSortAlgorithm);
 		return -1;
 	}
 }
--- src/main/java/com/example/springin5steps/SpringIn5StepsApplication.java
+++ src/main/java/com/example/springin5steps/SpringIn5StepsApplication.java
@@ -6,16 +6,17 @@ import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringIn5StepsApplication {
-
 	public static void main(String[] args) {
 //		BinarySearchImpl binarySearch = new BinarySearchImpl();
 		ConfigurableApplicationContext applicationContext =
 				SpringApplication.run(SpringIn5StepsApplication.class, args);
 		
 		BinarySearchImpl binarySearch = applicationContext.getBean(BinarySearchImpl.class);
-		
+		BinarySearchImpl binarySearch1 = applicationContext.getBean(BinarySearchImpl.class);
+
+		System.out.println("binarySearch = "+binarySearch);
+		System.out.println("binarySearch = "+binarySearch1);
 		int result = binarySearch.binarySearch(new int[] {12,3,5}, 3);
 		System.out.println(result);
 	}
-
 }
